a0c153c6f1861fbc4e2c5c16b7beb62dc77f7414
Ameet Jadhav <ammugs11@gmail.com>
2022-07-14 12:45:33 +0530

    added Employee Is Present Or Absent

--- src/com/bridgelabz/UC1_EmployeeWages.java
+++ src/com/bridgelabz/UC1_EmployeeWages.java
@@ -1,6 +1,16 @@
 package com.bridgelabz;
 
 public class UC1_EmployeeWages {
-	
+	public static void main(String[] args) {
 
+		// Constants
+		int IS_FULL_TIME = 1;
+
+	
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+		   System.out.println("Employee is Present");
+		else
+		   System.out.println("Employee is Abscent");
+		}
 }
